d07c8651d96fed15129704fc6cd5b024b9fc2006
Thibaut-Arnoux <thib.arnoux@gmail.com>
2024-07-23 11:41:38 +0200

    ci: add js custom action to delete tag

---
+++ .github/actions/docker-hub-delete-tags/index.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core');
+const http = require('@actions/http-client');
+
+import { HttpClient } from '@actions/http-client';
+import { PersonalAccessTokenCredentialHandler } from '@actions/http-client/lib/auth';
+
+async function getToken(username, password, version) {
+    const httpClient = new HttpClient('docker-hub-delete-tags');
+    const baseUrl = `https://hub.docker.com/${version}`;
+
+    const jsonObj = await httpClient.postJson(
+        `${baseUrl}/users/login`,
+        {
+            username,
+            password
+        }
+    );
+
+    return jsonObj.result.token;
+}
+
+async function deleteTag(username, repository, tag, token) {
+    const accessTokenHandler = new PersonalAccessTokenCredentialHandler(token);
+    const httpClient = new HttpClient(
+        'docker-hub-delete-tags',
+        [accessTokenHandler],
+    );
+    const baseUrl = `https://hub.docker.com/${version}`;
+
+    const result = await httpClient.del(`${baseUrl}/repositories/${username}/${repository}/tags/${tag}`);
+
+    if (result.message.statusCode !== 204) {
+        throw new Error(`Failed to delete tag : ${tag}`);
+    }
+}
+
+async function run() {
+    // extract inputs
+    const username = core.getInput('username', { required: true });
+    const password = core.getInput('password', { required: true });
+    const repository = core.getInput('repository', { required: true });
+    const tag = core.getInput('tag', { required: true });
+    const version = core.getInput('version', { required: true });
+
+    const token = await getToken(username, password, version);
+    await deleteTag(username, repository, tag, token);
+}
+
+run();
