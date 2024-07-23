48c29d81654a0fd53a0e4ca243a6106b9a4d0ea3
Thibaut-Arnoux <thib.arnoux@gmail.com>
2024-07-23 12:19:31 +0200

    Feat/pipeline ci (#4)
    * ci: init ci pipeline
    
    * ci: add output
    
    * ci: display only label from meta action
    
    * ci: log tags
    
    * ci: test build / push ci image to docker hub
    
    * ci: remove manual dispatch
    
    * ci: push image for ci
    
    * ci: github action unsupported dynamic container image name, use ci as tag
    
    * ci: use ci as static tag
    
    * ci: add pint step for ci
    
    * ci: add static analysis
    
    * ci: add pest test run
    
    * ci: add environment to load secret
    
    * ci: empty .env.testing to fix warning
    
    * ci: add xdebug in coverage mode to run pest coverage restriction
    
    * ci: use compact mode for coverage
    
    * ci: add todo for next steps
    
    * test: add pest test cases with database requirements
    
    * ci: init pgsql database service for running test
    
    * ci: typo on postres service name
    
    * ci: fix typo on secret for db connexion
    
    * ci: echo github context to manage unique image name'
    
    * ci: add some logs on ci
    
    * ci: test dynamic tag from pull request number
    
    * ci: init delete ci tag on closed pr
    
    * ci: add job to delete tag generate for ci
    
    * ci: add js custom action to delete tag
    
    * ci: use require synthax for js action
    
    * ci: add missing version parameter
    
    * ci: add logs
    
    * ci: more logs
    
    * ci: new logs
    
    * ci: use bearer token instead of personal token
    
    * ci: detach clean job from run-ci

---
+++ .github/actions/docker-hub-delete-tags/index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const http = require('@actions/http-client');
+const auth = require('@actions/http-client/lib/auth');
+
+async function getToken(username, password, version) {
+    const httpClient = new http.HttpClient('docker-hub-delete-tags');
+    const baseUrl = `https://hub.docker.com/${version}`;
+
+    const jsonObj = await httpClient.postJson(
+        `${baseUrl}/users/login`,
+        {
+            username,
+            password
+        }
+    );
+
+    return jsonObj.result.token;
+}
+
+async function deleteTag(username, repository, tag, token, version) {
+    const accessTokenHandler = new auth.BearerCredentialHandler(token);
+    const httpClient = new http.HttpClient(
+        'docker-hub-delete-tags',
+        [accessTokenHandler],
+    );
+    const baseUrl = `https://hub.docker.com/${version}`;
+
+    const result = await httpClient.del(`${baseUrl}/repositories/${username}/${repository}/tags/${tag}`);
+
+    if (result.message.statusCode !== 204) {
+        throw new Error(`Failed to delete tag : ${tag}`);
+    }
+}
+
+async function run() {
+    // extract inputs
+    const username = core.getInput('username', { required: true });
+    const password = core.getInput('password', { required: true });
+    const repository = core.getInput('repository', { required: true });
+    const tag = core.getInput('tag', { required: true });
+    const version = core.getInput('version', { required: true });
+
+    const token = await getToken(username, password, version);
+    await deleteTag(username, repository, tag, token, version);
+}
+
+run();
