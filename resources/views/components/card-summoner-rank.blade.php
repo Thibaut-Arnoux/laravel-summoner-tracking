d3e420bf994b086472fc0c01d60f00af441fed8e
Thibaut-Arnoux <thib.arnoux@gmail.com>
2024-10-29 21:50:51 +0100

    wip

---
+++ resources/views/components/card-summoner-rank.blade.php
@@ -0,0 +1,25 @@
+@use('Illuminate\Support\Number')
+@props(['queueType', 'imgSrc', 'tier', 'rank', 'wins', 'losses', 'lp'])
+
+<div class="w-full min-w-80 rounded-lg shadow-lg border border-primary overflow-hidden">
+    <div class="text-sm font-semibold px-4 py-2">
+        {{ $queueType }}
+    </div>
+    <hr />
+    <div class="flex flex-row justify-between p-4 gap-4 bg-base-100">
+        <div class="w-24 rounded-full border border-primary bg-base-200 overflow-hidden">
+            <img class="object-cover w-full h-full p-2" src={{ $imgSrc }} alt="Rank Icon">
+        </div>
+        <div class="flex flex-wrap">
+            <div class="flex justify-between items-center w-full">
+                <div class="text-xl font-bold">{{ $tier }} {{ $rank }}</div>
+                <div class="text-sm">{{ $wins }}V {{ $losses }}D</div>
+            </div>
+
+            <div class="flex justify-between w-full items-center">
+                <div class="text-sm">{{ $lp }} LP</div>
+                <div class="text-sm">Winrate {{ Number::percentage(($wins / ($wins + $losses)) * 100) }}</div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -8,28 +8,9 @@
     @if ($account)
         <livewire:components.feature.profile-summoner :puuid="$account->puuid" :$regionTag />
 
-        <div class="m-4"></div>
-
-        {{-- TODO : Move to component --}}
-        <div class="w-80 rounded-lg shadow-lg border border-primary overflow-hidden">
-            <div class="text-sm font-semibold px-4 py-2">
-                Classé en solo/duo
-            </div>
-            <hr />
-            <div class="flex flex-row p-4 gap-4 bg-base-100">
-                <img src="https://placehold.co/64" alt="Rank Icon" class="w-24 h-24 rounded-full">
-                <div class="w-full flex flex-wrap ">
-                    <div class="flex justify-between items-center w-full">
-                        <div class="text-xl font-bold">Gold 4</div>
-                        <div class="text-sm">14V 9D</div>
-                    </div>
-
-                    <div class="flex justify-between w-full items-center">
-                        <div class="text-sm">95 LP</div>
-                        <div class="text-sm">Taux de Victoire 61%</div>
-                    </div>
-                </div>
-            </div>
+        <div class="mt-4 w-80">
+            <x-card-summoner-rank queueType="Ranked Solo 5x5" :imgSrc="Vite::asset('resources/images/tier/gold.png')" tier="Gold" rank="4" wins="14"
+                losses="9" lp="95" />
         </div>
     @endif
 </div>
